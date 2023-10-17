436ac3cd0b2ebb1aa46d6f74c54ff146e39f8c83
DrakoAnastasia <ad80023072@gmail.com>
2023-10-17 22:06:11 +0300

    Add lesson 20 homework 1

---
+++ Homeworks_java/src/Lesson20/User.java
@@ -0,0 +1,42 @@
+package Lesson20;
+
+public class User {
+    private String name;
+    private int age;
+    private int id;
+
+    public User(String name, int age, int id) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + id + "Name: " + name + ", age: " + age;
+    }
+}
